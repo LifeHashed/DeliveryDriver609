cde5226a40b77e14ef7d0217ff04cb5cb2db79e4
LifeHashed <121927281+LifeHashed@users.noreply.github.com>
2023-06-13 22:54:15 +0530

    Update FollowCamera.cs

--- FollowCamera.cs
+++ FollowCamera.cs
@@ -1,1 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] GameObject thingToFollow;
+    // position should be same as car position
+    
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = thingToFollow.transform.position+ new Vector3(0,0,-10);
+    }
+}
 
