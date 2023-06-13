0a36b55b468a109fd726c277c2f1d7fbc79d85ce
LifeHashed <121927281+LifeHashed@users.noreply.github.com>
2023-06-13 22:49:57 +0530

    Update Driver.cs

--- Driver.cs
+++ Driver.cs
@@ -1,1 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Driver : MonoBehaviour
+{
+    [SerializeField]float steerspeed= 80.0f;
+    [SerializeField]float movespeed= 5.0f;
+    [SerializeField]float slowspeed= 2.0f;
+    [SerializeField]float boostspeed= 15.0f;
+
+    
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal")*steerspeed*Time.deltaTime;
+        float moveAmount=Input.GetAxis("Vertical")*movespeed*Time.deltaTime;
+
+
+        transform.Rotate(0,0,-steerAmount);
+        transform.Translate(0,moveAmount,0);
+    }
+
+    
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag=="Boost")
+        {
+            movespeed=boostspeed;
+        }
+
+        else if(other.tag=="Bump")
+        {
+            movespeed=slowspeed;
+        }
+    }
+
+    
+
+}
