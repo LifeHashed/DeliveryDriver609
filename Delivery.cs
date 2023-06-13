ae6cea51d0af75f82fb9d91d013aee5bdba535c4
LifeHashed <121927281+LifeHashed@users.noreply.github.com>
2023-06-13 22:51:46 +0530

    Update Delivery.cs

--- Delivery.cs
+++ Delivery.cs
@@ -1,1 +1,43 @@
+//Delivery system
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+public class Delivery: MonoBehaviour
+{
+    [SerializeField] Color32 hasPackageColor = new Color32(1,1,1,1);
+    [SerializeField] Color32 noPackageColor = new Color32(1,1,1,1);
+    
+    [SerializeField] float destroyDelay=0.5f;
+    bool hasPackage=false;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("Accident!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.tag=="Package"&&(!hasPackage))
+        {
+            Debug.Log("Package picked up");
+            hasPackage=true;
+            spriteRenderer.color = hasPackageColor;
+            Destroy(other.gameObject,destroyDelay);
+        }
+
+        if(other.tag=="Delivery"&&hasPackage)
+        {
+            Debug.Log("Package Delivered");
+            hasPackage=false;
+            spriteRenderer.color = noPackageColor;
+        }
+    }
+}
